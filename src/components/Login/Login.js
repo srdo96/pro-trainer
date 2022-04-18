f454d7783dc89b8128f2a9e0520caf37d5aff201
Sakib Rahman <seu.sakib@gmail.com>
2022-04-18 14:28:24 +0600

    implimant signInWithEmailAndPassword

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
+import Loading from "../Loading/Loading";
 
 const Login = () => {
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  let from = location.state?.from?.pathname || "/home";
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const pass = e.target.pass.value;
+    console.log(email, pass);
+    signInWithEmailAndPassword(email, pass);
+  };
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+  if (loading) {
+    <Loading></Loading>;
+  }
   return (
     <div className="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100 ">
       <div className="relative sm:max-w-sm w-full">
@@ -11,11 +34,12 @@ const Login = () => {
           <label className="block mt-2 text-2xl text-gray-700 text-center font-semibold">
             Login
           </label>
-          <form className="mt-5">
+          <form onSubmit={handleLogin} className="mt-5">
             <div className="mt-7">
               <input
                 type="email"
                 placeholder="Email"
+                name="email"
                 className="pl-3 mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
               />
             </div>
@@ -24,11 +48,12 @@ const Login = () => {
               <input
                 type="password"
                 placeholder="Password"
+                name="pass"
                 className="pl-3 mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
               />
             </div>
-
             <div className="mt-7">
+              {error && <p className="text-red-500 mb-4">{error.message}</p>}
               <button className="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
                 Login
               </button>
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { async } from "@firebase/util";
+import { sendEmailVerification } from "firebase/auth";
+import Loading from "../Loading/Loading";
 
 const Signup = () => {
   const [createUserWithEmailAndPassword, user, loading, error] =
-    useCreateUserWithEmailAndPassword(auth);
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
   const [uiError, setUiError] = useState("");
+  const navigate = useNavigate();
   const handleSignUp = async (e) => {
     e.preventDefault();
     const email = e.target.email.value;
@@ -22,9 +25,15 @@ const Signup = () => {
     }
     setUiError("");
     await createUserWithEmailAndPassword(email, pass);
-    toast("Registration complete");
+    // navigate("/home");
     console.log(user);
   };
+  if (loading) {
+    return <Loading></Loading>;
+  }
+  if (user) {
+    toast("Registration complete");
+  }
   return (
     <div className="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100 ">
       <div className="relative sm:max-w-sm w-full">
@@ -101,26 +110,6 @@ const Signup = () => {
             </div>
 
             <div className="mt-7">
-              {loading && (
-                <div className="text-center mb-2">
-                  <svg
-                    role="status"
-                    class="inline mr-2 w-8 h-8 text-gray-200 animate-spin fill-red-600"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentFill"
-                    />
-                  </svg>
-                </div>
-              )}
               {error && <p className="text-red-500">{error.message}</p>}
               <button
                 type="submit"
