5ebf0da79eeef6cc84d298407446008d28948af6
Sakib Rahman <seu.sakib@gmail.com>
2022-04-18 22:34:14 +0600

    fix banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,8 +3,19 @@ import banner from "../../img/cover0.png";
 
 const Banner = () => {
   return (
-    <div>
-      <img className="" src={banner} alt="" />
+    <div className="relative">
+      <div className="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
+        <div
+          className="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20"
+          data-carousel-item=""
+        >
+          <img
+            src={banner}
+            className="block absolute lg:top-80 top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+            alt="..."
+          />
+        </div>
+      </div>
     </div>
   );
 };
